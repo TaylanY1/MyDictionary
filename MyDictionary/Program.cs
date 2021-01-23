ee859d6e478dbf024444a775aa35d9abdb39ffe7
Taylan Yavrutürk <tyavruturk.1@gmail.com>
2021-01-23 19:20:25 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> cities = new MyDictionary<string, string>();
+
+            cities.Add("06", "Ankara");
+            cities.Add("35", "Izmir");
+            cities.Add("34", "Istanbul");
+            cities.Add("07", "Antalya");
+            cities.Add("33", "Mersin");
+
+            cities.GetAll();
+        }
+    }
+}
